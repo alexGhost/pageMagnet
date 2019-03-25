037e8f04f3fb4cc644a4861e8d865fcb9636126b
alexGhost <46798730+alexGhost@users.noreply.github.com>
2019-03-25 17:12:14 -0400

    Update site.js

--- site.js
+++ site.js
@@ -1,1 +1,16 @@
-alert('Hello World')
+var Greeter = /** @class */ (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("world");
+var button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function () {
+    alert(greeter.greet());
+};
+document.body.appendChild(button);
